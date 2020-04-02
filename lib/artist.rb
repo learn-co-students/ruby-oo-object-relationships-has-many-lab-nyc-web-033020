b56ca0733bc3aa6f180e68da16a8ab7646ac70f2
junko911 <junkotahara911@gmail.com>
2020-04-01 23:44:34 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+    attr_accessor :name
+    
+    def initialize(name)
+        @name = name
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+    
+    def add_song_by_name(song)
+        new_song = Song.new(song)
+        new_song.artist = self
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+        end
+    end
+
+    def self.song_count
+        Song.all.length
+    end
+end
