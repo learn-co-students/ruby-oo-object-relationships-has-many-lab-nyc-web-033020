466e277ff9f68ee2f7489cb763cf4659f0bf6720
Garen Sahagian <g.s.sahagian@gmail.com>
2020-04-02 17:29:49 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
 
-    attr_accessor :name
+    attr_accessor :name, :artist
     def initialize(name)
         @name=name
 
@@ -12,11 +12,16 @@ class Artist
 
 
     def add_song(song)
-        Song.new(song)
-        song.artist = self
+        
+       song.artist = self
     end
 
     def add_song_by_name(name)
+        Song.new(name)
         add_song(name)
+    end
+
+    def self.song_count
+        Song.all.count
     end
 end
